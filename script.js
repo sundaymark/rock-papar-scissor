df282259e7dda6ede234a9f17716610e5889091e
sundaymark <sundayukpai1122@gmail.com>
2022-06-25 22:23:45 +0100

    add the files above

--- script.js
+++ script.js
@@ -7,8 +7,14 @@ function playRound(playerSelection,computerSelection){
     if(playerSelection === computerSelection){
         return " it is a tie";
     }
-       
-    
+
+    if(
+        (playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'scissors' && computerSelection === 'papar')||
+        (playerSelection === 'papar' && computerSelection === 'rock')
+    ){
+        return "you win the computer"
+    }
 }
     const playerSelection = "rock";
     const computerSelection = computerPlay()
