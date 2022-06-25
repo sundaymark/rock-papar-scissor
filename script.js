ea6c47447e595c45e66dacb6b7d25e58106dc574
sundaymark <sundayukpai1122@gmail.com>
2022-06-25 23:08:35 +0100

    add the file above

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-function computerPlay(){
-    const select = ["rock","papar","scissors"]
-    return select[Math.floor(Math.random()* select.length)];
+function computerPlay() {
+    const select = ["rock", "papar", "scissors"]
+    return select[Math.floor(Math.random() * select.length)];
 }
 
-function playRound(playerSelection,computerSelection){
-    if(playerSelection === computerSelection){
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
         return " it is a tie";
     }
 
-    if(
-        (playerSelection === 'rock' && computerSelection === 'scissors') ||
-        (playerSelection === 'scissors' && computerSelection === 'papar')||
-        (playerSelection === 'papar' && computerSelection === 'rock')
-    ){
-        return "you win the computer"
+    if (
+        (playerSelection === 'rock' && computerSelection === 'scissors')
+    ) {
+        return "you win rock beat scissors"
+    }
+
+    if (playerSelection === 'papar' && computerSelection === 'rock') {
+        return "you win papar beat rock"
     }
 }
-    const playerSelection = "rock";
-    const computerSelection = computerPlay()
-    console.log (playRound(playerSelection,computerSelection));
+const playerSelection = "rock";
+const computerSelection = computerPlay()
+console.log(playRound(playerSelection, computerSelection));
 
 
