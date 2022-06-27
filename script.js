b834d824e294120ed6f883d610c38c66c54e6d76
sundaymark <sundayukpai1122@gmail.com>
2022-06-27 21:16:28 +0100

    add the file above

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function playRound(playerSelection, computerSelection) {
     }
 
     if (
-        (playerSelection === 'rock' && computerSelection === 'scissors')
+        (playerSelection === 'rock' && computerSelection === 'scissor')
     ) {
         return "you win rock beat scissors"
     }
@@ -17,8 +17,26 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection === 'papar' && computerSelection === 'rock') {
         return "you win papar beat rock"
     }
+
+    if (playerSelection === 'scissor' && computerSelection === 'papar') {
+        return "you win scissor beat pappar"
+    }
+    //computer selection section
+    if (computerSelection === 'rock' && playerSelection === 'scissor') {
+        return "you loss rock beat scissor"
+    }
+
+    if (computerSelection === 'papar' && playerSelection === 'rock') {
+        return "you loss papar beat rock"
+    }
+
+    if (computerSelection === 'scissor' && playerSelection === 'papar') {
+        return "you loss scissor beat papar "
+    }
+
+
 }
-const playerSelection = "rock";
+const playerSelection = "papar";
 const computerSelection = computerPlay()
 console.log(playRound(playerSelection, computerSelection));
 
